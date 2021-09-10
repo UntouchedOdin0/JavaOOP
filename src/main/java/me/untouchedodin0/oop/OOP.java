2ac53ddd81aa7008e9cf8774242b236d471223fa
UntouchedOdin0 <kylehicksuk@gmail.com>
2021-09-10 15:07:13 +0100

    Changed up the User class Added some generic lists

--- src/main/java/me/untouchedodin0/oop/OOP.java
+++ src/main/java/me/untouchedodin0/oop/OOP.java
@@ -1,5 +1,8 @@
 package me.untouchedodin0.oop;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class OOP {
 
     /*
@@ -16,11 +19,14 @@ public class OOP {
         The object class e.g. "User" is like a template, it's a cookie cutter what can be used to make a bunch
         more cookies. Just remember you won't use the object class but you'll use it to create the objects of
         which you can use.
+
+        Use constructors to force proper data.
      */
 
     public static void main(String[] args) {
         System.out.println("Hello world"); // Hello world is a argument
-        User user = new User(); // Creating a new User object.
+        User user = new User("Caleb", "Gold"); // Creating a new User object.
+
 //        User user2 = new User(); // Creating another User Object
 
         /*
@@ -28,9 +34,18 @@ public class OOP {
          */
 
         user.setName("Caleb");
-        user.membership = "Gold";
+        user.set_membership(User.Membership.Gold);
 
+        ArrayList<User> users = new ArrayList<>();
+        users.add(new User("Caleb"));
+        users.add(new User("Jason"));
 
-        System.out.println(user.getName() + " " + user.membership);
+        for (User u : users) {
+            System.out.println(u.getName());
+        }
+
+//        System.out.println(user);
+//        System.out.println(user2);
+//        System.out.println(5);
     }
 }
--- src/main/java/me/untouchedodin0/oop/User.java
+++ src/main/java/me/untouchedodin0/oop/User.java
@@ -2,16 +2,45 @@ package me.untouchedodin0.oop;
 
 public class User {
 
-    private String name;
-    String membership;
+    private String _name;
+    private String _membership = "Bronze";
+
+    public String toString() {
+        return getName() + " " + getMembership();
+    }
+
+    public User(String name) {
+        setName(name);
+    }
+
+    public User(String name, String membership) {
+        setName(name);
+        setMembership(membership);
+    }
 
     // If the args parameters has the same name as the variable, prefix it with "this"
 
     void setName(String name) {
-        this.name = name;
+        _name = name;
     }
 
     String getName() {
-        return name;
+        return _name;
+    }
+
+    void setMembership(String membership) {
+        _membership = membership;
+    }
+
+    void set_membership(Membership membership) {
+        _membership = membership.name();
+    }
+
+    public enum Membership {
+        Bronze, Silver, Gold;
+    }
+
+    String getMembership() {
+        return _membership;
     }
 }
